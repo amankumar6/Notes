ad5859aa27d2224b080ea5336d6a0ad4246b77ea
amankumar6 <amankumar786dpsdh@gmail.com>
2021-01-25 10:44:18 +0530

    typescript functions

--- TypeScript/main.js
+++ TypeScript/main.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-var message = 'Hello World';
+var message = "Hello World";
 console.log(message);
 var isBeginner = true;
 var total = 0;
@@ -11,14 +11,13 @@ var n = null;
 var u = undefined;
 var list1 = [1, 2, 3, 4, 5, 6, 7];
 var list2 = [1, 2, 3, 4];
-var person1 = ['Chrics', 22];
+var person1 = ["Chrics", 22];
 var Color;
 (function (Color) {
     Color[Color["Red"] = 10] = "Red";
     Color[Color["Green"] = 11] = "Green";
     Color[Color["Blue"] = 12] = "Blue";
 })(Color || (Color = {}));
-;
 var c = Color.Green;
 console.log(c);
 var randomValue = 10;
@@ -30,11 +29,35 @@ console.log(myVar1.name);
 // myVar1.toUpperCase();
 var myVar2 = 10;
 function hasName(obj) {
-    return !!obj &&
-        typeof obj === "object" &&
-        "name" in obj;
+    return !!obj && typeof obj === "object" && "name" in obj;
 }
 if (hasName(myVar2)) {
     console.log(myVar2.name);
 }
-myVar2.toUpperCase();
+// (myVar2 as string).toUpperCase();
+var multiType;
+multiType = 10;
+multiType = false;
+var anytype;
+anytype = 20;
+anytype = true;
+function add(num1, num2) {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+function addAlt(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    return num1 + num2;
+}
+console.log(add(5, 10));
+console.log(addAlt(3));
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: "Bruse",
+    lastName: "Wayne"
+};
+fullName(p);
--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -1,5 +1,5 @@
-export { }
-let message = 'Hello World'
+export {};
+let message = "Hello World";
 console.log(message);
 
 let isBeginner: boolean = true;
@@ -7,7 +7,7 @@ let total: number = 0;
 let name: string = "Aman";
 
 let sentence: string = `My name is ${name}
-I am a beginner in TypeScript`
+I am a beginner in TypeScript`;
 
 console.log(sentence);
 
@@ -15,18 +15,22 @@ let n: null = null;
 let u: undefined = undefined;
 
 let list1: number[] = [1, 2, 3, 4, 5, 6, 7];
-let list2: Array<number> = [1, 2, 3, 4]
+let list2: Array<number> = [1, 2, 3, 4];
 
-let person1: [string, number] = ['Chrics', 22]
+let person1: [string, number] = ["Chrics", 22];
 
-enum Color { Red = 10, Green, Blue };
+enum Color {
+  Red = 10,
+  Green,
+  Blue,
+}
 
-let c: Color = Color.Green
-console.log(c)
+let c: Color = Color.Green;
+console.log(c);
 
 let randomValue: any = 10;
 randomValue = false;
-console.log(randomValue)
+console.log(randomValue);
 
 let myVar1: any = 10;
 console.log(myVar1.name);
@@ -36,12 +40,46 @@ console.log(myVar1.name);
 let myVar2: unknown = 10;
 
 function hasName(obj: any): obj is { name: string } {
-    return !!obj &&
-        typeof obj === "object" &&
-        "name" in obj
+  return !!obj && typeof obj === "object" && "name" in obj;
 }
 
 if (hasName(myVar2)) {
-    console.log(myVar2.name);
+  console.log(myVar2.name);
 }
-(myVar2 as string).toUpperCase();
+// (myVar2 as string).toUpperCase();
+
+let multiType: number | boolean;
+multiType = 10;
+multiType = false;
+
+let anytype: any;
+anytype = 20;
+anytype = true;
+
+function add(num1: number, num2?: number): number {
+  if (num2) return num1 + num2;
+  else return num1;
+}
+
+function addAlt(num1: number, num2: number = 10): number {
+  return num1 + num2;
+}
+
+console.log(add(5, 10));
+console.log(addAlt(3));
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function fullName(person: Person) {
+  console.log(`${person.firstName} ${person.lastName}`);
+}
+
+let p = {
+  firstName: "Bruse",
+  lastName: "Wayne",
+};
+
+fullName(p);
